088db5ccf20e770a85142cacbf4d009f8632f9d5
Grégoire Sage <gregoire.sage@gmail.com>
2020-11-24 00:23:04 +0100

    Add mask option for screenshots

--- packages/sdk-cli/src/commands/screenshot.ts
+++ packages/sdk-cli/src/commands/screenshot.ts
@@ -4,6 +4,8 @@ import dateformat from 'dateformat';
 import untildify from 'untildify';
 import vorpal from '@moleculer/vorpal';
 import open from 'open';
+import { PNG } from 'pngjs';
+import { mergeMaskByFile, outputAsync } from 'alpha-mask';
 
 import HostConnections from '../models/HostConnections';
 import captureScreenshot from '../models/captureScreenshot';
@@ -17,49 +19,64 @@ export default function screenshot(stores: {
         'screenshot [path] [--open]',
         'Capture a screenshot from the connected device',
       )
-      .types({ string: ['path'] })
+      .types({ string: ['path', 'mask'] })
       .option(
         '-o, --open',
         'Opens the screenshot using the native picture viewer',
       )
-      .action(async (args: vorpal.Args & { path?: string; open?: boolean }) => {
-        const { appHost } = stores.hostConnections;
-        if (!appHost) {
-          cli.activeCommand.log('Not connected to a device');
-          return false;
-        }
-
-        const destPath = path.resolve(
-          args.path
-            ? untildify(args.path)
-            : dateformat('"Screenshot" yyyy-mm-dd "at" H.MM.ss."png"'),
-        );
-
-        try {
-          await captureScreenshot(appHost.host, destPath, {
-            onWrite(received, total) {
-              cli.ui.redraw(
-                total == null
-                  ? 'Downloading...'
-                  : `Downloading: ${Math.round(
-                      (received / total) * 100,
-                    )}% completed`,
-              );
-            },
-          });
-
-          cli.ui.redraw(`Screenshot saved to ${destPath}`);
-
-          if (args.options.open) {
-            await open(destPath, { wait: false });
+      .option(
+        '--mask <mask>',
+        'Masks the screenshot using the given image (B&W)',
+      )
+      .action(
+        async (
+          args: vorpal.Args & { path?: string; open?: boolean; mask?: string },
+        ) => {
+          const { appHost } = stores.hostConnections;
+          if (!appHost) {
+            cli.activeCommand.log('Not connected to a device');
+            return false;
           }
-          return true;
-        } catch (ex) {
-          cli.ui.redraw(String(ex));
-          return false;
-        } finally {
-          cli.ui.redraw.done();
-        }
-      });
+
+          const destPath = path.resolve(
+            args.path
+              ? untildify(args.path)
+              : dateformat('"Screenshot" yyyy-mm-dd "at" H.MM.ss."png"'),
+          );
+
+          try {
+            await captureScreenshot(appHost.host, destPath, {
+              onWrite(received, total) {
+                cli.ui.redraw(
+                  total == null
+                    ? 'Downloading...'
+                    : `Downloading: ${Math.round(
+                        (received / total) * 100,
+                      )}% completed`,
+                );
+              },
+            });
+
+            if (args.options.mask) {
+              const maskPath = path.resolve(untildify(args.options.mask));
+              await mergeMaskByFile(destPath, maskPath).then((res: PNG) =>
+                outputAsync(destPath, res),
+              );
+            }
+
+            cli.ui.redraw(`Screenshot saved to ${destPath}`);
+
+            if (args.options.open) {
+              await open(destPath, { wait: false });
+            }
+            return true;
+          } catch (ex) {
+            cli.ui.redraw(String(ex));
+            return false;
+          } finally {
+            cli.ui.redraw.done();
+          }
+        },
+      );
   };
 }
